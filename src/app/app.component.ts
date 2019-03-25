babbaffb2a861c78ec288f3942916ab1968ba865
IanSani <iansani259@gmail.com>
2019-03-25 12:09:03 +0300

    count component added for voting buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule } from '@angular/material';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { HighlightDirective } from './highlight.directive';
+import { FormComponent } from './form/form.component';
+import { DateCountPipe } from './date-count.pipe';
+import { VoteComponent } from './vote/vote.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    HighlightDirective,
+    FormComponent,
+    DateCountPipe,
+    VoteComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppComponent {
-  title = 'Quotes';
-}
+export class AppModule { }
