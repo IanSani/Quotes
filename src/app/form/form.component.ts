5f17d968f759a24c9fd4c279f546036cecbcb8e1
IanSani <iansani259@gmail.com>
2019-03-25 11:34:00 +0300

    linked form.component.ts to other components

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote'//transmitting to quote component(parent)
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
 })
-export class FormComponent implements OnInit {
+export class QuoteFormComponent implements OnInit {
+
+  newQuote = new Quote(0, "", "", "", new Date());//format as defined by class property
+
+  @Output() addQuote = new EventEmitter<Quote>();//event emitter to listen and send out new quotes on quote parent component
+
+  submitQuote() {
+    this.addQuote.emit(this.newQuote);//this triggers addition of the new quote
+  }
 
   constructor() { }
 
